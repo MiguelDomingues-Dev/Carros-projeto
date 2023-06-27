c6983b3136a7a41e1f676cf6c607e52bf0d50830
MiguelDomingues-Dev <85039271+MiguelDomingues-Dev@users.noreply.github.com>
2023-06-26 21:45:00 -0300

    att

--- pais.js
+++ pais.js
@@ -1,24 +1,17 @@
   function enter() {
     var txt1 = window.document.getElementById('txtnome')
     var txt2 = window.document.getElementById('txtnome2') 
-    var res = window.document.getElementById('res')
-    var dodgeCharger = 328
-    var ferrari = 340
-    var mustang = 250
-    var supra = 250
-    var lamborghin = 350
+    var res = window.document.getElementById('result')
+    var x = "ferrari"
+    var y = "mustang"
     var text1 = (txt1.value)
     var text2 = (txt2.value)
     var n1 = Number(text1.value)
     var n2 = Number(text2.value)
-    var d = dodgeCharger || ferrari || mustang || supra || lamborghin
-    var e = n1 >= n2
-    var f = n2 >= n1
-    var s = e == f  
-      res.innerHTML = `O carro vencedor foi o ${s}`
-    
-    // }else {
-    //   if (text2 =! text1) {
-    //     res.innerHTML = `O carro vencedor foi o ${s}`
-    //   }
+    if (n1 > n2) {
+      res.innerHTML = `O carro vencedor foi o ${x}`
+    } else {
+      n1 < n2 
+      res.innerHTML = `O carro vencedor foi o ${y}`
+    }
   }
